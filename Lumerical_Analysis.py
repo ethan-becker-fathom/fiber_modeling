b94c2d6230c6d99eb77b32685ac441286536f26d
Ethan Becker <ethan@fathomradiant.co>
2023-02-02 11:16:49 -0700

    imrpove lumerical analysis class.
    Should be good to run simulations now. Need to test multithreading this to start multiuple lumerical instances.

--- Lumerical_Analysis.py
+++ Lumerical_Analysis.py
@@ -22,8 +22,10 @@ class NumpyArrayEncoder(JSONEncoder):
 
 
 class Lumerical_Mode:
-    def __int__(self, named_parameters=None):
+    def __init__(self, named_parameters=None, sellmeier_b=None, sellmeier_c=None, n=None):
         self.lum = None
+
+        print('init')
 
         self.default_analysis_parameters = {
             'maximum number of modes to store': 6,
@@ -31,24 +33,22 @@ class Lumerical_Mode:
             'convergence tolerance': 1e-12
         }
 
-        if named_parameters is None:
-            self.named_parameters = {
-                "core": {
-                    "radius": core_radius,
-                    'index': n + deltan_core
-                },
-                "ring": {
-                    "inner radius": trench_ID,
-                    "outer radius": trench_OD,
-                    'index': n - deltan_ring
-                },
-                'clad': {
-                    'index', n
-                }
-            }
+        self.named_parameters = named_parameters
 
-        self.wavelength = None
+        self.wavelength_nm = None
         self.bend_radius = None
+
+        self.sellmeier_b = np.array([0.6961663, 0.4079426, 0.8974794])
+        if sellmeier_b is not None:
+            self.sellmeier_b = sellmeier_b
+
+        self.sellmeier_c = np.array([0.0684043, 0.1162414, 9.896161])
+        if sellmeier_c is not None:
+            self.sellmeier_c = sellmeier_c
+
+        self.n = 1.45
+        if n is not None:
+            self.n = n
 
     def open(self, hidden=False):
         self.lum = lumapi.MODE(hide=hidden)
@@ -56,22 +56,35 @@ class Lumerical_Mode:
     def load_model(self, model_path):
         self.lum.load(model_path)
 
-    def set_bending_radius(self, bending_radius:float=0):
+    def set_bending_radius(self, bending_radius: float = 0):
         logging.debug(f'Setting Bending Radius to {bending_radius}')
         self.bend_radius = bending_radius
         if bending_radius == 0:
             self.set_analysis_parameter('bent waveguide', 0)
         else:
-            set_lumerical_analysis_parameter('bent waveguide', 1)
-            set_lumerical_analysis_parameter('bend radius', self.bend_radius)
+            self.set_analysis_parameter('bent waveguide', 1)
+            self.set_analysis_parameter('bend radius', self.bend_radius)
 
+    def set_wavelength(self, wavelength_nm, update_refractive_indices: bool = True):
+        logging.debug(f'Setting Wavelength to {wavelength_nm}_nm')
+        self.wavelength_nm = wavelength_nm
+        wavelength_m = self.wavelength_nm / 1e9
+        self.set_analysis_parameter('wavelength', wavelength_m)
 
-    def set_wavelength(self, wavelength, update_refractive_indices: bool = True):
-        logging.debug(f'Setting Wavelength to {wavelength}')
-        self.wavelength = wavelength
-        self.set_analysis_parameter('wavelength', self.wavelength)
+        if update_refractive_indices:
+            wavelength_mu = self.wavelength_nm / 1e3
 
+            n = np.sqrt(((Sellmeier_B * np.square(wavelength_mu)) / (
+                    np.square(wavelength_mu) - np.square(Sellmeier_C))).sum() + 1)
 
+            self.n = n
+            self.named_parameters['clad']['index'] = n
+            for name, params in self.named_parameters.items():
+                for param, value in params.items():
+                    if param == '*delta_index':
+                        params['index'] = n + value
+
+            self.set_named_parameters()
 
     def get_analysis_parameters(self, log=True):
         params = lum.getanalysis().split('\n')
@@ -92,15 +105,18 @@ class Lumerical_Mode:
         for param, value in self.default_analysis_parameters.items():
             self.set_analysis_parameter(param, value)
 
-    def set_named_parameters(self):
-        for name, params in self.named_parameters.items():
-            for param, value in params.items():
-                self.set_named_parameter(name, param, value)
-
     def set_named_parameter(self, name: str, parameter: str, value):
         self.lum.switchtolayout()
         logging.debug(f'Setting named parameter {name},{parameter}: {value}')
         lum.setnamed(name, parameter, value)
+
+    def set_named_parameters(self):
+        for name, params in self.named_parameters.items():
+            for param, value in params.items():
+                if param.startswith('*'):
+                    continue
+                self.set_named_parameter(name, param, value)
+
 
     def find_modes(self):
         logging.debug(f'Finding Modes')
@@ -139,6 +155,8 @@ class Lumerical_Mode:
         return modes
 
     def get_mode_summary(self, save_to_file: bool = True, folder: Path = Path()):
+        logging.debug(f'Getting Mode Summary')
+
         modes = {}
         modes['wavelength'] = self.wavelength_nm
         modes['bend_radius'] = self.bend_radius
